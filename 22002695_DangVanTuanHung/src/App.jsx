dcc0a7250b98afbca90f9965e035ca0ec048472b
H82M19\Student <Student>
2026-01-28 10:16:13 +0700

    App

--- 22002695_DangVanTuanHung/src/App.jsx
+++ 22002695_DangVanTuanHung/src/App.jsx
@@ -1,30 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserForm from "./UserForm";
+import DigitalClock from "./DigitalClock";
 
 function App() {
-  const [text, setText] = useState('');
-  const [name, setName] = useState('');
-
-  function inText(e){
-    setText(e.target.value);
-  }
-
-  function Click(){
-    setName(text);
-  }
-
-
   return (
     <>
-      <input onChange={inText} type="text" />
-
-      <button onClick={Click}>Click</button>
-      <br />
-      <span>Hello {name}</span>
+      <UserForm />
+      <hr />
+      <DigitalClock />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
